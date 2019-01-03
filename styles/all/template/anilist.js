10d140d03e378c0d22eaa27778a6495c5e5f5398
johnelliotbaker <johnelliotbaker@gmail.com>
2019-01-02 18:32:28 -0800

    Change opening modal to simulated click
    Modal was using fade in, but was not being activated because i worked by
    capturing click event.
    Restore css for :target and simulate click with javascript.

--- styles/all/template/anilist.js
+++ styles/all/template/anilist.js
@@ -10,53 +10,72 @@ function getEntryOrEmpty(template, text, url=0)
 
 }
 
+function getEndDateOrEmpty(template, endDate, url=0)
+{
+    var year = endDate['year'];
+    if (year)
+    {
+        var dateString = `${endDate['year']}/${endDate['month']}/${endDate['day']}`
+        return getEntryOrEmpty(template, dateString, url);
+    }
+    else
+        return "";
+}
+
+function getRatingOrEmpty(template, rating, url=0)
+{
+    if (rating > 0)
+    {
+        return getEntryOrEmpty(template, (rating/10).toFixed(1), url);
+    }
+    return "";
+}
+
 function makeAnilistTemplate(data)
 {
     console.log(data);
-    var type = data['type'];
-    var id = data['id'];
-    var url = data['siteUrl'];
+    var type          = data['type'];
+    var id            = data['id'];
+    var url           = data['siteUrl'];
 
-    var img          = getEntryOrEmpty(`[center][img width="300"]{text}[/img][/center]\n`, data['coverImage']['large']);
-    var bannerImage  = data['bannerImage'] ? getEntryOrEmpty(`[center][img width="300"]{text}[/img][/center]\n`, data['bannerImage']) : "";
-    // var year         = getEntryOrEmpty(`[center][size=150][color=#000000][b]({text})[/b][/color][/size][/center]\n`, data['startDate']['year']);
-    var year = data['startDate']['year'] ? ' (' + data['startDate']['year'] + ')' : "";
-    var titleNative  = getEntryOrEmpty(`[center][size=200][b][url={url}]{text}${year}[/url][/b][/size][/center]\n`, data['title']['native'], url);
-    var titleRomaji  = getEntryOrEmpty(`[center][size=100][b]{text}[/b][/size][/center]\n`, data['title']['romaji']);
-    var titleEnglish = getEntryOrEmpty(`[center][size=100][b]{text}[/b][/size][/center]\n`, data['title']['english']);
+    var img           = getEntryOrEmpty(`[center][img width="300"]{text}[/img][/center]\n`, data['coverImage']['large']);
+    var bannerImage   = data['bannerImage'] ? getEntryOrEmpty(`[center][img width="300"]{text}[/img][/center]\n`, data['bannerImage']) : "";
+    var year          = data['startDate']['year'] ? ' (' + data['startDate']['year'] + ')' : "";
+    var titleNative   = getEntryOrEmpty(`[center][size=200][b][url={url}]{text}${year}[/url][/b][/size][/center]\n`, data['title']['native'], url);
+    var titleRomaji   = getEntryOrEmpty(`[center][size=100][b]{text}[/b][/size][/center]\n`, data['title']['romaji']);
+    var titleEnglish  = getEntryOrEmpty(`[center][size=100][b]{text}[/b][/size][/center]\n`, data['title']['english']);
+    var rating        = getRatingOrEmpty(`[center][size=150][b]{text} / 10[/b][/size][/center]\n`, data['averageScore']);
+    var genre         = getEntryOrEmpty(`[center][b][size=120]{text}[/size][/b][/center]\n`, data['genres'].join(', '));
+    var summary       = getEntryOrEmpty(`[quote][center]{text}[/center][/quote]\n`, data['description']);
 
-    var rating       = getEntryOrEmpty(`[center][size=150][b]Rating: {text} / 10[/b][/size][/center]\n`, (data['averageScore']/10).toFixed(1));
-
-    var genre        = getEntryOrEmpty(`[center][b][size=120]{text}[/size][/b][/center]\n`, data['genres'].join(', '));
-
-    var summary      = getEntryOrEmpty(`[quote][center]{text}[/center][/quote]\n`, data['description']);
-
-    var volumes     = getEntryOrEmpty(`[color=#FF8000][b]Volumes[/b][/color]: {text}\n`, data['volumes']);
-    var trailer = "";
-    try 
-    {
-        var trailer = getEntryOrEmpty(`[color=#FF8000][b]Trailer[/b][/color]: [url=https://www.youtube.com/watch?v={url}]{text}[/url]\n`, 'Youtube', data['trailer']['id'])
-    }catch{}
-    var episodes     = getEntryOrEmpty(`[color=#FF8000][b]Episodes[/b][/color]: {text}\n`, data['episodes']);
-    var chapters     = getEntryOrEmpty(`[color=#FF8000][b]Chapters[/b][/color]: {text}\n`, data['chapters']);
-    var runtime      = getEntryOrEmpty(`[color=#FF8000][b]Runtime[/b][/color]: {text} minutes\n`, data['duration']);
-    var votes        = getEntryOrEmpty(`[color=#FF8000][b]Votes[/b][/color]: {text}\n`, numberWithCommas(data['popularity']));
-    var links        = `[color=#FF8000][b]Links[/b][/color]: [b]`;
-    var ddl          = `[color=#0000FF][b]Direct Download Links[/color][/b]: \n`;
-    var dlink        = `[hide][b][url=https://links.snahp.it/xxxx][color=#FF0000]MEGA[/color][/url]
+    var volumes       = getEntryOrEmpty(`[color=#FF8000][b]Volumes[/b][/color]: {text}\n`, data['volumes']);
+    var trailer       = "";
+    try { var trailer = getEntryOrEmpty(`[color=#FF8000][b]Trailer[/b][/color]: [url=https://www.youtube.com/watch?v={url}]{text}[/url]\n`, 'Youtube', data['trailer']['id']) } catch {};
+    var episodes      = getEntryOrEmpty(`[color=#FF8000][b]Episodes[/b][/color]: {text}\n`, data['episodes']);
+    var endDate       = getEndDateOrEmpty(`[color=#FF8000][b]End Date[/b][/color]: {text}\n`, data['endDate']);
+    var chapters      = getEntryOrEmpty(`[color=#FF8000][b]Chapters[/b][/color]: {text}\n`, data['chapters']);
+    var runtime       = getEntryOrEmpty(`[color=#FF8000][b]Runtime[/b][/color]: {text} minutes\n`, data['duration']);
+    var votes         = getEntryOrEmpty(`[color=#FF8000][b]Votes[/b][/color]: {text}\n`, numberWithCommas(data['popularity']));
+    var links         = `[color=#FF8000][b]Links[/b][/color]: [b]`;
+    var ddl           = `[color=#0000FF][b]Direct Download Links[/color][/b]: \n`;
+    var dlink         = `[hide][b][url=https://links.snahp.it/xxxx][color=#FF0000]MEGA[/color][/url]
 [url=https://links.snahp.it/xxxx][color=#FF0000]ZippyShare[/color][/url]
 [url=https://snahp.it/?s=tt1270797][color=#FF0000]ZippyShare[/color][/url]
 [/b][/hide]\n`
-    var text = img + bannerImage + '\n\n';
-    if (titleNative && titleNative != 'null') text += titleNative + '\n';
-    if (titleRomaji && titleRomaji != 'null') text += titleRomaji + '\n';
-    if (titleEnglish && titleEnglish != 'null') text += titleEnglish + '\n';
+    var text = '' + 
+        bannerImage + '\n\n\n' + 
+        img + '\n\n\n';
+        if (titleNative && titleNative != 'null') text += titleNative + '\n';
+        if (titleRomaji && titleRomaji != 'null') text += titleRomaji + '\n';
+        if (titleEnglish && titleEnglish != 'null') text += titleEnglish + '\n';
     text += '\n\n' + rating + '\n\n\n' +
         genre + '\n\n' +
         summary + '\n\n' +
-        runtime + episodes + volumes + chapters +
+        runtime + episodes + volumes + chapters + endDate +
         votes + trailer + '\n' +
         ddl + dlink;
+    text = text.replace(/(<br>|<br\/>|<br \/>)/g, '');
+    text = text.replace(/(\r?\n|\r){5,99}/g, '\n\n\n\n');
     return text;
 }
 
@@ -88,7 +107,7 @@ function updatePosters(media)
         $imgDiv = $('<div/>')
             .addClass('img_container')
             .appendTo($li);
-        $img = $('<img class="rounded"/>')
+        $img = $('<img/>')
             .attr({
                 "id": "img-" + count,
                 "src": entry["coverImage"]["large"],
@@ -122,10 +141,12 @@ function updatePosters(media)
         $notfound = $("<h>Not Found</h>").css({"font-size": "200%"});
         $anilist_content.append($notfound);
     }
-    $("#anilist_dialog").css({
-        "opacity": "1",
-        "pointer-events": "auto"
-    });
+    $('<a href="#anilist_dialog" id="modalTriggerLink"></a>').appendTo($("body"));
+    setTimeout(function(){
+        $("#modalTriggerLink")[0].click();
+        $("#modalTriggerLink").remove();
+    }, 100);
+    // $("#anilist_dialog").css({ "opacity": "1", "pointer-events": "auto" });
 }
 
 function filterMedia(media)
@@ -152,8 +173,8 @@ function filterMedia(media)
 
 var anilist_dialog_template = `
 <div id="anilist_dialog" class="modalDialog">
-  <div class="twbs card document rounded">
-    <div class="twbs card-body rounded">
+  <div class="twbs card document rounded" style="border: 10px; border-color:white;">
+    <div class="twbs card-body">
       <div class="twbs card-title">
         <h5 id="anilist_title"></h5>
       </div>
@@ -225,6 +246,11 @@ function startHandlingAnilistAjax()
           startDate {
             year
           },
+          endDate {
+            year
+            month
+            day
+          }
           episodes,
           chapters,
           duration,
