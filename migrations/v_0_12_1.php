c1249ced2d1e3601b55b565165e120c87d4a33dc
johnelliotbaker <johnelliotbaker@gmail.com>
2019-02-01 23:09:10 -0800

    Add index to phpbb_snahp_request

---
+++ migrations/v_0_12_1.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * snahp. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2018
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace jeb\snahp\migrations;
+
+class v_0_12_1 extends \phpbb\db\migration\migration
+{
+    public function effectively_installed()
+    {
+        return false;
+    }
+
+    static public function depends_on()
+    {
+        return array(
+            '\jeb\snahp\migrations\v_0_12_0',
+        );
+    }
+
+    public function update_schema()
+    {
+        return array(
+            'add_index'	=> array(
+                $this->table_prefix . 'snahp_request'  => array(
+                    'status' => array('status'),
+                    'fid'    => array('fid'),
+                ),
+            ),
+        );
+    }
+
+}
