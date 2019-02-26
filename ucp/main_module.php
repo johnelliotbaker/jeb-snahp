fccc5a35dbf4977fa4cb0bde4a8fa847992f9c5f
johnelliotbaker <johnelliotbaker@gmail.com>
2019-02-25 18:47:12 -0800

    Fix a bug in ucp
    In ucp->snahp setting, submitting the form was giving sql error.
    This was because a variable was moved to the end of the main_module.php
    function to increase efficiency. The variable was needed, however,
    so it was moved back.

--- ucp/main_module.php
+++ ucp/main_module.php
@@ -53,6 +53,10 @@ class main_module
         $this->tpl_name = $cfg['tpl_name'];
         $this->page_title = $user->lang('UCP_SNP_TITLE');
         add_form_key('jeb/snahp');
+        $data = [
+            'snp_disable_avatar_thanks_link' => $request->variable('snp_disable_avatar_thanks_link', $user->data['snp_disable_avatar_thanks_link']),
+            'snp_enable_at_notify'           => $request->variable('snp_enable_at_notify', $user->data['snp_enable_at_notify']),
+        ];
         if ($request->is_set_post('submit'))
         {
             if (!check_form_key('jeb/snahp'))
@@ -72,10 +76,6 @@ class main_module
             }
         }
         // Receive thanks notifications
-        $data = [
-            'snp_disable_avatar_thanks_link' => $request->variable('snp_disable_avatar_thanks_link', $user->data['snp_disable_avatar_thanks_link']),
-            'snp_enable_at_notify'           => $request->variable('snp_enable_at_notify', $user->data['snp_enable_at_notify']),
-        ];
         $template_vars = array(
             'S_SNP_DISABLE_AVATAR_THANKS_LINK' => $data['snp_disable_avatar_thanks_link'],
             'S_SNP_ENABLE_AT_NOTIFY'           => $data['snp_enable_at_notify'],
