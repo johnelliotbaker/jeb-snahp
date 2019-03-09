d310dd20a6b868d37106a241d5ff0ffe2904cf4d
johnelliotbaker <johnelliotbaker@gmail.com>
2019-03-09 15:50:18 -0800

    Git did not properly merge reqperc branch
    Re-merge

--- controller/favorite.php
+++ controller/favorite.php
@@ -142,6 +142,39 @@ class favorite extends base
         $tpl_name = $cfg['tpl_name'];
         if ($tpl_name)
         {
+            // Temporarily hard code some forums
+            // TODO: Find a better way to get these
+            $a_img_url = [
+                'imported' => 'https://i.imgur.com/9RLP1KV.png',
+                'apps' => 'https://i.imgur.com/7hjHY25.png',
+                'games' => 'https://i.imgur.com/YBPkBaX.png',
+                'tv' => 'https://i.imgur.com/i9pIvNy.png',
+                'movies' => 'https://i.imgur.com/pK0D79T.png',
+                'music' => 'https://i.imgur.com/g5r9eAI.png',
+                'anime' => 'https://i.imgur.com/BpFV059.png',
+                'misc' => 'https://i.imgur.com/X4OIR7c.png',
+                'dev' => 'https://i.imgur.com/OU5XZDq.png',
+            ];
+            $a_category = [
+                // Test Server
+                // 'imported' => 50, 'apps' => 51, 'games' => 52,
+                // 'movies' => 82, 'tv' => 53, 'music' => 54,
+                // 'anime' => 55, 'misc' => 56, 'dev' => 57,
+                // PRODUCTION Server
+                'imported' => 27, 'apps' => 9, 'games' => 10,
+                'movies' => 11, 'tv' => 12, 'music' => 14,
+                'anime' => 13, 'misc' => 15, 'dev' => 76,
+            ];
+            $fid_lookup = [];
+            foreach ($a_category as $name => $fid)
+            {
+                $a_fid = $this->select_subforum($fid, 3600);
+                foreach ($a_fid as $f)
+                {
+                    $fid_lookup[$f] = $a_img_url[$name];
+                }
+            }
+
             $base_url = $cfg['base_url'];
             $fid_listings = $this->config['snp_fid_listings'];
             $pagination = $this->container->get('pagination');
@@ -154,6 +187,15 @@ class favorite extends base
             );
             foreach ($data as $row)
             {
+                $forum_id = $row['forum_id'];
+                if (isset($fid_lookup[$forum_id]))
+                {
+                    $img_url = $fid_lookup[$forum_id];
+                }
+                else
+                {
+                    $img_url = '';
+                }
                 $tid = $row['topic_id'];
                 $topic_time = $this->user->format_date($row['topic_time']);
                 $u_details = '/viewtopic.php?t=' . $tid;
@@ -181,11 +223,14 @@ class favorite extends base
                     'TOPIC_VIEWS'    => $row['topic_views'],
                     'FORUM_NAME'     => $row['forum_name'],
                     'REPLIES'        => $row['topic_posts_approved'] - 1,
+                    'U_IMG_URL'      => $img_url,
                     'U_VIEW_DETAILS' => $u_details,
                 );
                 $this->template->assign_block_vars('postrow', $group);
             }
-            $this->template->assign_var('TITLE', $cfg['title']);
+            $this->template->assign_vars([
+                'TITLE' => $cfg['title'],
+            ]);
             return $this->helper->render($tpl_name, $cfg['title']);
         }
     }
