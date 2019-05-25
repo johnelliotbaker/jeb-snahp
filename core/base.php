0c3c06604bdbb88e2996b36fe8372ccde54b13c9
johnelliotbaker <johnelliotbaker@gmail.com>
2019-05-25 02:28:12 -0700

    Allow zs and gd to be iconized

--- core/base.php
+++ core/base.php
@@ -929,9 +929,9 @@ abstract class base
     {
         $ptn = '#(\[(mega)\])#is';
         $strn = preg_replace($ptn, '<img class="mega_icon" src="https://i.imgur.com/w5aP33F.png">', $strn);
-        $ptn = '#(\[(gdrive)\])#is';
+        $ptn = '#(\[(gdrive|gd)\])#is';
         $strn = preg_replace($ptn, '<img class="gdrive_icon" src="https://i.imgur.com/VQv2dUm.png">', $strn);
-        $ptn = '#(\[(zippy)\])#is';
+        $ptn = '#(\[(zippy|zs|zippyshare)\])#is';
         $strn = preg_replace($ptn, '<img class="zippy_icon" src="https://i.imgur.com/uJXaUNG.png">', $strn);
         return $strn;
     }
