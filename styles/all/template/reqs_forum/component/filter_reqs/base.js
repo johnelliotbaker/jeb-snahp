baa794322258e9924290ef42e88d13c63d015db8
johnelliotbaker <johnelliotbaker@gmail.com>
2019-06-08 21:14:55 -0700

    Fix problem where if the cookie was undefined (after cache purge) all requests were hidden

--- styles/all/template/reqs_forum/component/filter_reqs/base.js
+++ styles/all/template/reqs_forum/component/filter_reqs/base.js
@@ -1,22 +1,21 @@
 var Filter_reqs = {};
 
-Filter_reqs.select_tags = function(mode)
+Filter_reqs.select_tags = function(selection)
 {
-    Cookie.set('requests', 'filter.selection', mode);
-    // createCookie('requests_selector_cookie', mode, 365);
-    Filter_reqs.hide_tags(mode);
+    Cookie.set('requests', 'filter.selection', selection);
+    Filter_reqs.hide_tags(selection);
 }
 
-Filter_reqs.hide_tags = function(mode)
+Filter_reqs.hide_tags = function(selection='all')
 {
     $a_btn = $('.btn');
     $.each($a_btn, (index)=>{
         $btn = $($a_btn[index]);
-        var b_match = $btn.hasClass(mode) && !$btn.hasClass('selector');
+        var b_match = $btn.hasClass(selection) && !$btn.hasClass('selector');
         $elem = $btn.closest('li');
         if ($elem)
         {
-            if (b_match || mode=='all')
+            if (b_match || selection=='all')
             {
                 $elem.removeClass('hidden');
             }
@@ -28,10 +27,10 @@ Filter_reqs.hide_tags = function(mode)
     });
 }
 
-var cookie = Cookie.get('requests', 'filter.selection');
-if (!cookie)
+var selection = Cookie.get('requests', 'filter.selection');
+if (!selection)
 {
     var selection = 'all';
     Cookie.set('requests', 'filter.selection', selection);
 }
-Filter_reqs.hide_tags(cookie);
+Filter_reqs.hide_tags(selection);
