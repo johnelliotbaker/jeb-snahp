19aef6ec29981c5075fd7866bf3cdce03ace69da
johnelliotbaker <johnelliotbaker@gmail.com>
2019-06-29 20:30:56 -0700

    Start snahp reputation
    Make migrations

---
+++ migrations/v_0_27_0.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace jeb\snahp\migrations;
+
+class v_0_27_0 extends \phpbb\db\migration\migration
+{
+    public function effectively_installed()
+    { return false; }
+
+    static public function depends_on()
+    { return ['\jeb\snahp\migrations\v_0_26_0']; }
+
+    public function update_schema()
+    {
+        return [
+            'add_tables' => [
+                $this->table_prefix . 'snahp_reputation' => [
+                    'COLUMNS' => [
+                        'id'        => ['UINT', null, 'auto_increment'],
+                        'post_id'   => ['INT:11', 0],
+                        'poster_id' => ['INT:11', 0],
+                        'giver_id'  => ['INT:11', 0],
+                        'time'      => ['INT:11', 0],
+                    ],
+                    'PRIMARY_KEY' => 'id',
+                ],
+            ],
+            'add_index'    => [
+                $this->table_prefix . 'snahp_reputation'  => [
+                    'post_id' => ['post_id'],
+                    'poster_id' => ['poster_id'],
+                    'giver_id' => ['giver_id'],
+                ],
+            ],
+            'add_columns' => [
+                USERS_TABLE => [
+                    'snp_rep_n_received'  => ['INT:11', 0],
+                    'snp_rep_n_given'     => ['INT:11', 0],
+                    'snp_rep_n_available' => ['INT:11', 5],
+                    'snp_rep_b_avatar'    => ['BOOL', 1],
+                ],
+            ],
+        ];
+    }
+
+    public function revert_schema()
+    {
+        return [
+            'drop_columns' => [
+                USERS_TABLE => [
+                    'snp_rep_n_received',
+                    'snp_rep_n_given',
+                    'snp_rep_n_available',
+                    'snp_rep_b_avatar',
+                ],
+            ],
+            'drop_tables' => [
+                $this->table_prefix . 'snahp_reputation',
+            ],
+        ];
+    }
+
+    public function update_data()
+    {
+        return [
+            ['config.add', ['snp_rep_b_master', 1]],
+            ['config.add', ['snp_rep_b_avatar', 1]],
+            ['config.add', ['snp_rep_b_toplist', 1]],
+        ];
+    }
+
+}
