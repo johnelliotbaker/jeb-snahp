259d6bcb02b29f19091100e46872c64ecb25fd66
johnelliotbaker <johnelliotbaker@gmail.com>
2019-08-24 13:37:10 -0700

    Improve Spinners

--- styles/all/template/economy/mcp/user_manager/component/assets/base.js
+++ styles/all/template/economy/mcp/user_manager/component/assets/base.js
@@ -14,7 +14,14 @@ UserAssets.save_user_balance = function(event)
     $user_balance = $('#user_balance');
     var balance = $user_balance.val();
     var url = '/app.php/snahp/economy/uam/set_user_balance/?u=' + user_id + '&b=' + balance;
+    $('#user_balance_button').prop('disabled', true);
+    $('#user_balance_spinner').css('display', '');
+    $('#user_balance_text').text('Saving...');
     $.get(url).done((resp)=>{
-        // location.reload();
+        setTimeout(function() {
+            $('#user_balance_spinner').css('display', 'none');
+            $('#user_balance_text').text('Save');
+            $('#user_balance_button').prop('disabled', false);
+        }.bind(this), 1000);
     });
 }
