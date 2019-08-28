c699b10d675eb76513e73d8f7e7a428eb739ad29
johnelliotbaker <johnelliotbaker@gmail.com>
2019-08-27 23:09:00 -0700

    Refine imgcompare
    When dimenions were in certain way, tall images would create a modal
    window with large white blank space to the right.
    The modal window itself would shift toward the left.

--- core/template/imgcompare.php
+++ core/template/imgcompare.php
@@ -23,12 +23,10 @@ class imgcompare
         if (!filter_var($url1, FILTER_VALIDATE_URL) || !filter_var($url2, FILTER_VALIDATE_URL)) { 
             return 'First two arguments must be valid URL.';
         }
-        $uuid = uniqid('imgcompare_');
-        $head = '
-<div onClick="send_resize_event();"
-	class="imgcompare_thumbnail_container pointer noselect"
-    data-toggle="modal"
-    data-target="#' . $uuid . '">
+        $html = '
+<div 
+onClick="ImgCompare.setup_modal(' . "'$url1', '$url2', '$label1', '$label2'" . ')"
+	class="imgcompare_thumbnail_container pointer noselect">
     <div class="crop_top">
         <img src="'. $url1 .'" class="imgcompare_thumbnail_left">
     </div>
@@ -36,81 +34,8 @@ class imgcompare
         <img src="'. $url2 .'" class="imgcompare_thumbnail_right">
     </div>
 </div>
-<div class="twbs">
-  <div class="modal fade" id="' . $uuid . '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </div>
-        <div class="modal-body p-1">
 ';
-        $tail = '
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-';
-        $body = '
-          <div style="max-height:95vh;" id="slider1" class="beer-slider" data-beer-label="' . $label1 . '" data-start="50">
-            <img style="width:100%;" src="' . $url1 . '" alt="' . $label1 . '">
-            <div class="beer-reveal" data-beer-label="' . $label2 . '">
-              <img src="' . $url2 . '" alt="' . $label2 . '">
-            </div>
-          </div>
-';
-        $res = [$head, $body, $tail];
-        $html = join(PHP_EOL, $res);
         return $html;
-    }/*}}}*/
-
-    private function handle_grid($data, $options=[])/*{{{*/
-    {
-        $column_size = $this->def['column_sizes'][$options['size']];
-        $html['begin'][] = '
-<link rel="stylesheet" type="text/css" href="/ext/jeb/snahp/styles/all/template/gallery/component/grid/base.css">
-<div class="twbs">
-<section class="gallery-block grid-gallery">
-	<div class="container">
-		<div class="row">
-';
-        $html['end'][] = '
-    </div>
-  </div>
-</section>
-</div>';
-        $ptn = '<dl class="hidebox (\w+)">';
-        $class = ['', ' hi'];
-        $elem = ['a', 'span'];
-        foreach($data as $d)
-        {
-            $link = strip_tags($d[2]);
-            $choice = 0;
-            preg_match($ptn, $d[2], $match);
-            if (count($match)>0)
-            {
-                if($match[1]=='hi')
-                {
-                    $choice = 1;
-                }
-            }
-            $cls = $class[$choice];
-            $el = $elem[$choice];
-            $body[] = '<div class="' . $column_size . ' item' . $cls . '"> 
-                           <' . $el . ' href="' . $link . '" target="_blank">
-                               <img class="img-fluid image scale-on-hover" src="' . $d[3] . '">
-                           </' . $el . '>
-                       </div>';
-        }
-        $html['body'] = $body;
-        $sequence = ['begin', 'body', 'end'];
-        $res = '';
-        foreach ($sequence as $key)
-        {
-            $res .= join(PHP_EOL, $html[$key]);
-        }
-        return $res;
     }/*}}}*/
 
 }
--- styles/all/template/imgcompare/base.js
+++ styles/all/template/imgcompare/base.js
@@ -1,38 +1,97 @@
-// https://github.com/kavyasukumar/imgSlider
-function send_resize_event()
+// Using the beerslider from
+// https://img.shields.io/github/size/pehaa/beerslider
+
+var ImgCompare = {};
+
+ImgCompare.img = {};
+
+ImgCompare.w2h = function(w, ar)
 {
-  for(let i=0; i<5; i++)
-  {
-    setTimeout(function(){
-      window.dispatchEvent(new Event('resize'));
-    }, 100*i);
-  }
+    return w/ar;
 }
 
-$(window).resize(()=>{
-  var vh = $(window).height();
-  var vw = $(window).width();
-  var h = vh*.8;
-  var w = vw*.8;
-  var img = document.getElementById("img1");
-  var nh = img.naturalHeight;
-  var nw = img.naturalWidth;
-  var ar = nw/nh;
-  var ih = img.height;
-  var iw = img.wight;
-  // img.width = w;
-  // $('.modal-dialog').css('max-width', '');
-  // $('.modal-dialog').css('width', w + 'px');
-})
+ImgCompare.h2w = function(h, ar)
+{
+    return h*ar;
+}
 
-$(document).ready(function() {
-  $.fn.BeerSlider = function( options ) {
-    options = options || {};
-    return this.each( function () {
-      new BeerSlider( this, options );
+ImgCompare.generate_modal_body = function(url1, url2, label1='', label2='')
+{
+    var tpl = `
+          <div class="beer-slider" data-beer-label="` + label1 + `" data-start="50">
+            <img class="image_left" src="` + url1 + `">
+            <div class="beer-reveal" data-beer-label="` + label2 + `">
+              <img id="image_right" src="` + url2 + `">
+            </div>
+          </div>`;
+    return tpl;
+}
+
+ImgCompare.setup_modal = function(url1, url2, label1='', label2='')
+{
+    $mbody = $('#imgcompare_modal .modal-body');
+    $mbody.html(ImgCompare.generate_modal_body(url1, url2, label1, label2));
+    $img1 = $('#imgcompare_modal .image_left');
+    $img2 = $('#imgcompare_modal .image_right');
+    $img1.prop('src', url1);
+    $img2.prop('src', url2);
+    $modal = $('#imgcompare_modal').modal('show');
+    this.img[0] = $img1;
+    this.img[1] = $img2;
+    setTimeout(function(){
+        ImgCompare.update_modal_window();
+        ImgCompare.initbeer();
+    }, 50);
+}
+
+ImgCompare.update_modal_window = function()
+{
+    $img1 = this.img[0];
+    var nh = $img1[0].naturalHeight;
+    var nw = $img1[0].naturalWidth;
+    var ar_n = nw/nh;
+    var vh = $(window).height()*.95;
+    var vw = $(window).width()*.90;
+    var ar_v = vw/vh;
+    var h, w;
+    if (ar_v > ar_n)
+    {
+        h = vh;
+        w = this.h2w(h, ar_n);
+    }
+    else
+    {
+        w = vw;
+        h = this.w2h(w, ar_n);
+    }
+    $modal_dialog = $('#imgcompare_modal .modal-dialog');
+    $modal_dialog.width(w);
+    $modal_dialog.height(h);
+    $modal_content = $('#imgcompare_modal .modal-content');
+    $modal_content.width('auto');
+    $modal_dialog.css('marginLeft', 'auto');
+    var ml = $modal_dialog.css('marginLeft');
+    var wc = $modal_content.width();
+    var wi = $img1.width();
+    ml = parseInt(ml.substring(0, ml.length-2));
+    if (wc > wi)
+    {
+        ml += (wc-wi)*.5;
+        $modal_dialog.css('marginLeft', ml);
+        $modal_content.width($img1.width());
+    }
+}
+
+ImgCompare.initbeer = function()
+{
+    $.fn.BeerSlider = function( options ) {
+        options = options || {};
+        return this.each( function () {
+            new BeerSlider( this, options );
+        });
+    };
+    $( ".beer-slider" ).each( function( index, el ) {
+        $( el ).BeerSlider( {start: $( el ).data( "start" ) } )
     });
-  };
-  $( ".beer-slider" ).each( function( index, el ) {
-    $( el ).BeerSlider( {start: $( el ).data( "start" ) } )
-  });
-});
+}
+
