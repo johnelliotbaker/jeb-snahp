f7e73a97f0b0510d849b43eee0bca795e50c93b8
johnelliotbaker <johnelliotbaker@gmail.com>
2019-12-01 01:44:56 -0800

    Disable seasonal until christmas

--- styles/all/template/seasonal.js
+++ styles/all/template/seasonal.js
@@ -5,38 +5,39 @@ Seasonal_theme.modify_elem_list = [ '.headerbar', '.site_logo', '.logo', '.site-
 
 Seasonal_theme.toggle_thanksgiving = function()
 {
+  var tg_enable = Cookie.get('seasonal', 'thanksgiving_enable');
+  if (tg_enable===undefined)
+  {
+    Cookie.set('seasonal', 'thanksgiving_enable', true);
     var tg_enable = Cookie.get('seasonal', 'thanksgiving_enable');
-    if (tg_enable===undefined)
-    {
-        Cookie.set('seasonal', 'thanksgiving_enable', true);
-        var tg_enable = Cookie.get('seasonal', 'thanksgiving_enable');
-    }
-    if (tg_enable)
-    {
-        Cookie.set('seasonal', 'thanksgiving_enable', false);
-    }
-    else
-    {
-        Cookie.set('seasonal', 'thanksgiving_enable', true);
-    }
-    window.location = '/';
+  }
+  if (tg_enable)
+  {
+    Cookie.set('seasonal', 'thanksgiving_enable', false);
+  }
+  else
+  {
+    Cookie.set('seasonal', 'thanksgiving_enable', true);
+  }
+  window.location = '/';
 }
 
 Seasonal_theme.apply_thanksgiving = function()
 {
-    var enable = Cookie.get('seasonal', 'thanksgiving_enable');
-    if (enable===undefined || enable)
+  return;
+  var enable = Cookie.get('seasonal', 'thanksgiving_enable');
+  if (enable===undefined || enable)
+  {
+    for (var elem_name of this.modify_elem_list)
     {
-        for (var elem_name of this.modify_elem_list)
-        {
-            $(elem_name).addClass('thanksgiving');
-        }
+      $(elem_name).addClass('thanksgiving');
     }
-    else
+  }
+  else
+  {
+    for (var elem_name of this.modify_elem_list)
     {
-        for (var elem_name of this.modify_elem_list)
-        {
-            $(elem_name).removeClass('thanksgiving');
-        }
+      $(elem_name).removeClass('thanksgiving');
     }
+  }
 }
