8b093199b55a39d555603d29707bb50ba6d1541f
johnelliotbaker <johnelliotbaker@gmail.com>
2019-12-22 22:49:40 -0800

    Add [imdb] [discogs] BBCodes

---
+++ migrations/v_0_36_2.php
@@ -0,0 +1,52 @@
+<?php
+
+// React Post Generators: IMDb & Discogs
+
+namespace jeb\snahp\migrations;
+
+use jeb\snahp\core\bbcodes_installer;
+use phpbb\db\migration\container_aware_migration;
+
+class v_0_36_2 extends container_aware_migration
+{
+    public function effectively_installed()
+    { return false; }
+
+    static public function depends_on()
+    { return ['\jeb\snahp\migrations\v_0_36_1']; }
+
+    public function update_schema()
+    {
+        return [ ];
+    }
+
+    public function revert_schema()
+    {
+        return [ ];
+    }
+
+	public function install_bbcodes()
+	{
+		$install = new bbcodes_installer($this->db, $this->container->get('request'), $this->container->get('user'), $this->phpbb_root_path, $this->php_ext);
+		$install->install_bbcodes([
+			'imdb' => [
+				'display_on_posting' => false,
+				'bbcode_match'       => '[imdb]{TEXT}[/imdb]',
+				'bbcode_tpl'         => '<div class="rx_imdb_template" data-data="{TEXT}"><div>',
+			],
+			'discogs' => [
+				'display_on_posting' => false,
+				'bbcode_match'       => '[discogs]{TEXT}[/discogs]',
+				'bbcode_tpl'         => '<div class="rx_discogs_template" data-data="{TEXT}"><div>',
+			],
+		]);
+	}
+
+    public function update_data()
+    {
+        return [
+			['custom', [[$this, 'install_bbcodes']]],
+        ];
+    }
+
+}
