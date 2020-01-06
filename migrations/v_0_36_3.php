14b118c02637b8d10cb488c4032b15b71c39bc77
johnelliotbaker <johnelliotbaker@gmail.com>
2020-01-06 01:09:43 -0800

    Migration to make multibyte charset on tpl

---
+++ migrations/v_0_36_3.php
@@ -0,0 +1,42 @@
+<?php
+
+// React Post Generators: IMDb & Discogs
+
+namespace jeb\snahp\migrations;
+
+use jeb\snahp\core\bbcodes_installer;
+use phpbb\db\migration\container_aware_migration;
+
+class v_0_36_3 extends container_aware_migration
+{
+    public function effectively_installed()
+    { return false; }
+
+    static public function depends_on()
+    { return ['\jeb\snahp\migrations\v_0_36_2']; }
+
+    public function update_schema()
+    {
+        return [ ];
+    }
+
+    public function revert_schema()
+    {
+        return [ ];
+    }
+
+    public function set_utf8mb4()
+    {
+        global $table_prefix, $db;
+        $sql = 'ALTER TABLE ' . $table_prefix . 'snahp_tpl CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci';
+        $db->sql_query($sql);
+    }
+
+    public function update_data()
+    {
+        return [
+            ['custom', [[$this, 'set_utf8mb4']]],
+        ];
+    }
+
+}
