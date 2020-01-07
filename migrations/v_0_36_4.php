6820d267e282dc7e06712030a783879ddb2760ee
johnelliotbaker <johnelliotbaker@gmail.com>
2020-01-06 23:32:15 -0800

    Add custom tpl priority

---
+++ migrations/v_0_36_4.php
@@ -0,0 +1,47 @@
+<?php
+
+// Foe Blocking
+
+namespace jeb\snahp\migrations;
+
+class v_0_36_4 extends \phpbb\db\migration\migration
+{
+    public function effectively_installed()
+    { return false; }
+
+    static public function depends_on()
+    { return ['\jeb\snahp\migrations\v_0_36_3']; }
+
+    public function update_schema()
+    {
+        return [
+            'add_columns' => [
+                $this->table_prefix . 'snahp_tpl' => [
+                    'priority'  => ['TINT:2', 0],
+                ],
+            ],
+            'add_index'    => [
+                $this->table_prefix . 'snahp_tpl' => [
+                    'priority' => ['priority'],
+                ],
+            ],
+        ];
+    }
+
+    public function revert_schema()
+    {
+        return [
+            'drop_columns' => [
+                $this->table_prefix . 'snahp_tpl' => [
+                    'priority',
+                ],
+            ],
+        ];
+    }
+
+    public function update_data()
+    {
+        return [ ];
+    }
+
+}
