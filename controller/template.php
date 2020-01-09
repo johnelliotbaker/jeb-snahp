4b7036016eba4597cd31f45ab0450603eab7719c
johnelliotbaker <johnelliotbaker@gmail.com>
2020-01-09 03:24:35 -0800

    Replace custom template app

--- controller/template.php
+++ controller/template.php
@@ -30,6 +30,10 @@ class template extends base
             $cfg = [];
             return $this->save_tpl($cfg);
             break;
+        case 'save_order':
+            $cfg = [];
+            return $this->save_tpl_order($cfg);
+            break;
         case 'create':
             $cfg = [];
             return $this->create_tpl($cfg);
@@ -38,36 +42,16 @@ class template extends base
             $cfg = [];
             return $this->del_tpl($cfg);
             break;
-        // case 'test':
-        //     $cfg = [];
-        //     return $this->test($cfg);
-        //     break;
-        // case 'test2':
-        //     $cfg = [];
-        //     return $this->test2($cfg);
-        //     break;
-        // case 'test_save':
-        //     $cfg = [];
-        //     return $this->test_save($cfg);
-        //     break;
-        // case 'test_delete':
-        //     $cfg = [];
-        //     return $this->test_delete($cfg);
-        //     break;
-        // case 'test_create':
-        //     $cfg = [];
-        //     return $this->test_create($cfg);
-        //     break;
         default:
             break;
         }
         trigger_error('Error Code: 62869cedad');
     }
 
-    private function reject_overdraw()
+    private function reject_overdraw($user_id)
     {
         $js = new \phpbb\json_response();
-        $user_id = $this->user->data['user_id'];
+        $user_id = (int) $user_id;
         $gid = $this->user->data['group_id'];
         $sql = 'SELECT snp_customtemplate_n_max FROM ' . GROUPS_TABLE . ' WHERE group_id = ' . $gid;
         $result = $this->db->sql_query($sql);
@@ -92,7 +76,7 @@ class template extends base
     {
         $js = new \phpbb\json_response();
         $user_id = $this->user->data['user_id'];
-        $this->reject_overdraw();
+        $this->reject_overdraw($user_id);
         $name = $this->request->variable('name', '');
         $name = preg_replace("/[^A-Za-z0-9_ ]/", '', $name);
         $text = $this->request->variable('text', '', true);
@@ -117,12 +101,28 @@ class template extends base
         $js->send(['status' => $sql ? 'success' : 'fail']);
     }
 
+    public function save_tpl_order($cfg)
+    {
+        $js = new \phpbb\json_response();
+        $user_id = $this->user->data['user_id'];
+        $items = json_decode(htmlspecialchars_decode($this->request->variable('items', '', true)), true);
+        foreach($items as $item)
+        {
+          $priority = (int) $item['priority'];
+          $name = $this->db->sql_escape($item['name']);
+          $sql = "UPDATE phpbb_snahp_tpl SET priority=${priority} WHERE user_id=${user_id} AND name='${name}';";
+          $this->db->sql_query($sql);
+        }
+        $status = true;
+        $js->send([ 'status' => $status ? 'success' : 'fail', ]);
+    }
+
     public function del_tpl($cfg)
     {
         $js = new \phpbb\json_response();
-        $u = $this->user->data['user_id'];
-        $n = $this->request->variable('n', '');
-        $sql = $this->delete_tpl($u, $n);
+        $user_id = $this->user->data['user_id'];
+        $name = $this->request->variable('n', '');
+        $sql = $this->delete_tpl($user_id, $name);
         $js->send(['status' => $sql ? 'success' : 'fail']);
     }
 
@@ -134,65 +134,6 @@ class template extends base
         $user_id = $this->user->data['user_id'];
         $rowset = $this->select_tpl($user_id, $b_full, $name);
         $js->send($rowset);
-    }
-
-    public function test($cfg)
-    {
-        $js = new \phpbb\json_response();
-        $b_full = (bool) $this->request->variable('full', false);
-        $name = $this->request->variable('name', '');
-        $user_id = 2;
-        $rowset = $this->select_tpl($user_id, $b_full, $name);
-        $js->send($rowset);
-    }
-
-    public function test2($cfg)
-    {
-        $js = new \phpbb\json_response();
-        $text = $this->request->variable('text', '', true);
-        $name = $this->request->variable('name', '', true);
-        $js->send(['text' => $text, 'name' => $name]);
-    }
-
-    public function test_save($cfg)
-    {
-        $user_id = 2;
-        $js = new \phpbb\json_response();
-        $name = $this->request->variable('name', '');
-        $text = $this->request->variable('text', '', true);
-        if ($text)
-        {
-            $sql = $this->update_tpl($user_id, $name, $text);
-        }
-        $js->send(['status' => 'success',
-            'user_id'=>$user_id,
-            'name'=>$name,
-            'text'=>$text]);
-    }
-
-    public function test_delete($cfg)
-    {
-        $u = 2;
-        $n = $this->request->variable('n', '');
-        $this->delete_tpl($u, $n);
-        $js = new \phpbb\json_response();
-        $js->send([ 'status' => 'success' ]);
-    }
-
-    public function test_create($cfg)
-    {
-        $js = new \phpbb\json_response();
-        $user_id = 2;
-        // $this->reject_overdraw();
-        $name = $this->request->variable('name', '');
-        $name = preg_replace("/[^A-Za-z0-9_ ]/", '', $name);
-        $text = $this->request->variable('text', '', true);
-        if ($text)
-        {
-          $sql = $this->upsert_tpl($user_id, $name, $text);
-        }
-        $js->send(['sql'=>$sql, 'status' => $sql ? 'success' : 'fail']);
-        $js->send(['status' => 'success', 'name'=>$name, 'text'=>$text]);
     }
 
 }
