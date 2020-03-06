daabbb60a06d5b11be2249ed416398e66ad4da16
johnelliotbaker <johnelliotbaker@gmail.com>
2020-03-06 07:02:41 -0800

    Add icon to remove link from topic title.
    So that users can copy portions of the title to copy text.

---
+++ styles/all/template/viewtopic.js
@@ -0,0 +1,40 @@
+const TopicTitleMenu = (function(){
+  return class {
+    constructor() {
+      this.setupTopicTitleMenu();
+      this.text = '';
+    }
+
+    setupTopicTitleMenu() {
+      this.topicTitleHeader = $('h2.topic-title');
+      this.prependDeboner();
+    }
+
+    prependClipboard() {
+      const clipboard = $('<i title="Copy Title" class="fa icon fa-clipboard noselect pointer" style="margin-right:0.2em;"></i>');
+      clipboard.click((e)=>{
+        Clipboard.copy(this.text);
+      })
+      this.topicTitleHeader.prepend(clipboard);
+    }
+
+    prependDeboner() {
+      const topicTitleLink = this.topicTitleHeader.find('a');
+      const unlink = $('<i title="Debone" class="fa icon fa-unlink noselect pointer" style="margin-right:0.2em;"></i>');
+      const text = topicTitleLink.text().slice();
+      this.topicTitleHeader.prepend(unlink);
+      unlink.click((e)=>{
+        topicTitleLink.replaceWith(()=>{
+          return $(`<span>${text}</span>`);
+        })
+        this.text = text;
+        this.prependClipboard();
+        unlink.remove();
+      });
+    }
+  }
+})();
+
+$(document).ready(function(){
+  const topicTitleMenu = new TopicTitleMenu();
+});
