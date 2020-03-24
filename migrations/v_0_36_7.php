2ab3c8f87f3cd8c94c83422c905b4713e1a3d04d
johnelliotbaker <johnelliotbaker@gmail.com>
2020-03-23 23:12:39 -0700

    Add "snahplyr" audio player
    Based on plyr.io
    uses
    [thehub]{ "items" : [
      {
        "title": "my title 1",
        "artist": "my artist 1",
        "src": "https://source.to/the/file1",
        "poster": "https://link.to/the/poster1"
      },
      {
        "title": "my title 2",
        "artist": "my artist 2",
        "src": "https://source.to/the/file2",
        "poster": "https://link.to/the/poster2"
      }
    ]}
    [/thehub]

---
+++ migrations/v_0_36_7.php
@@ -0,0 +1,48 @@
+<?php
+
+// React Post Generators: IMDb & Discogs
+
+namespace jeb\snahp\migrations;
+
+use jeb\snahp\core\bbcodes_installer;
+use phpbb\db\migration\container_aware_migration;
+
+class v_0_36_7 extends container_aware_migration
+{
+  public function effectively_installed()
+  { return false; }
+
+  static public function depends_on()
+  { return ['\jeb\snahp\migrations\v_0_36_6']; }
+
+  public function update_schema()
+  {
+    return [ ];
+  }
+
+  public function revert_schema()
+  {
+    return [ ];
+  }
+
+  public function install_bbcodes()
+  {
+    $install = new bbcodes_installer($this->db, $this->container->get('request'), $this->container->get('user'), $this->phpbb_root_path, $this->php_ext);
+    $install->install_bbcodes([
+      'thehub' => [
+        'display_on_posting' => false,
+        'bbcode_match'       => '[thehub]{TEXT}[/thehub]',
+        'bbcode_tpl'         => '<div class="rx_snahplyr" data-data="{TEXT}"><div>',
+      ],
+    ]);
+  }
+
+  public function update_data()
+  {
+    return [
+      ['custom', [[$this, 'install_bbcodes']]],
+    ];
+  }
+
+
+}
