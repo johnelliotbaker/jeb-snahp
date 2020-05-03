a564f5cdfbca1323f2e4a46af2354e10d2ae55e0
johnelliotbaker <johnelliotbaker@gmail.com>
2020-05-02 20:51:51 -0700

    Implement MassIndexer

---
+++ Apps/MassIndexer/MassIndexerController.php
@@ -0,0 +1,73 @@
+<?php
+namespace jeb\snahp\Apps\MassIndexer;
+
+use \Symfony\Component\HttpFoundation\Response;
+use \Symfony\Component\HttpFoundation\JsonResponse;
+use jeb\snahp\Apps\MassIndexer\MassIndexerHelper;
+
+class MassIndexerController
+{
+    protected $db;/*{{{*/
+    protected $user;
+    protected $config;
+    protected $request;
+    protected $template;
+    protected $container;
+    protected $helper;
+    protected $tbl;
+    protected $sauth;
+    protected $myHelper;
+    public function __construct(
+        $db,
+        $user,
+        $config,
+        $request,
+        $template,
+        $container,
+        $helper,
+        $tbl,
+        $sauth,
+        $myHelper
+    ) {
+        $this->db = $db;
+        $this->user = $user;
+        $this->config = $config;
+        $this->request = $request;
+        $this->template = $template;
+        $this->container = $container;
+        $this->helper = $helper;
+        $this->tbl = $tbl;
+        $this->sauth = $sauth;
+        $this->myHelper = $myHelper;
+        $this->userId = (int) $this->user->data['user_id'];
+        $this->sauth->reject_non_dev('Error Code: 75ae9f597c');
+    }/*}}}*/
+
+    public function unindexUser($username)/*{{{*/
+    {
+        $username = (string) $username;
+        $rootForum = $this->request->variable('f', 0);
+        $total = $this->myHelper->unindexAllPostsByUsername(
+            $username,
+            $rootForum
+        );
+        return $this->standardJsonResponse($total);
+    }/*}}}*/
+
+    public function unindexTopic($topicId)/*{{{*/
+    {
+        $topicId = (int) $topicId;
+        $total = $this->myHelper->unindexAllPostsByTopic($topicId);
+        return $this->standardJsonResponse($total);
+    }/*}}}*/
+
+    private function standardJsonResponse($total)/*{{{*/
+    {
+        return new JsonResponse(
+            [
+                "status" => "SUCCESS",
+                "total_processed" => $total,
+            ]
+        );
+    }/*}}}*/
+}
---
+++ Apps/MassIndexer/MassIndexerHelper.php
@@ -0,0 +1,164 @@
+<?php
+namespace jeb\snahp\Apps\MassIndexer;
+
+class MassIndexerHelper
+{
+    const CACHE_DURATION = 0;
+    const CACHE_DURATION_LONG = 0;
+    const CHUNK_SIZE = 50;
+
+    protected $db;/*{{{*/
+    protected $user;
+    protected $template;
+    protected $tbl;
+    protected $sauth;
+    protected $forumHelper;
+    public function __construct(
+        $db,
+        $user,
+        $template,
+        $tbl,
+        $sauth,
+        $forumHelper
+    ) {
+        $this->db = $db;
+        $this->user = $user;
+        $this->template = $template;
+        $this->tbl = $tbl;
+        $this->rxnTbl = $tbl['reaction'];
+        $this->sauth = $sauth;
+        $this->forumHelper = $forumHelper;
+        $this->user_id = $this->user->data['user_id'];
+    }/*}}}*/
+
+    public function unindexAllPostsByUsername($username, $rootForum=null)/*{{{*/
+    {
+        $userId = $this->sauth->userNameToUserId($username);
+        $this->rejectInvalidUser($userId, $username);
+        $this->rejectInvalidForum($rootForum);
+        $search = $this->getSearchInstance();
+        $rowset = $this->getPostsRelatedToUser($userId, $rootForum);
+        $postIds = array_map(
+            function ($row) {
+                return $row['post_id'];
+            },
+            $rowset
+        );
+        $total = count($postIds);
+        $chunks = array_chunk($postIds, $this::CHUNK_SIZE);
+        foreach ($chunks as $chunk) {
+            $search->index_remove($chunk, [], []);
+        }
+        return $total;
+    }/*}}}*/
+
+    private function getPostsRelatedToUser($userId, $rootForum)/*{{{*/
+    {
+        $where[] = "a.topic_poster={$userId}";
+        if ($rootForum) {
+            $subForums = $this->forumHelper->selectSubforum(
+                $rootForum
+            );
+            if ($subForums) {
+                $where[] = $this->db->sql_in_set('b.forum_id', $subForums);
+            }
+        }
+        $where = implode(' AND ', $where);
+        $sql_ary = [
+            'SELECT'   => 'b.post_id',
+            'FROM'     => [TOPICS_TABLE => 'a'],
+            'LEFT_JOIN' => [
+                [
+                    'FROM'  => [POSTS_TABLE => 'b'],
+                    'ON'    => 'a.topic_id=b.topic_id',
+                ],
+            ],
+            'WHERE'    => $where,
+        ];
+        $sql    = $this->db->sql_build_query('SELECT', $sql_ary);
+        $result = $this->db->sql_query($sql);
+        $rowset = $this->db->sql_fetchrowset($result);
+        $this->db->sql_freeresult($result);
+        return $rowset;
+    }/*}}}*/
+
+    private function getSearchInstance()/*{{{*/
+    {
+        global $phpbb_root_path, $phpEx, $auth, $config;
+        global $db, $user, $phpbb_dispatcher;
+        $search_type = $config['search_type'];
+        if (!class_exists($search_type)) {
+            trigger_error('NO_SUCH_SEARCH_MODULE');
+        }
+        $error = false;
+        return new $search_type(
+            $error,
+            $phpbb_root_path,
+            $phpEx,
+            $auth,
+            $config,
+            $db,
+            $user,
+            $phpbb_dispatcher
+        );
+    }/*}}}*/
+
+    private function rejectInvalidForum($forumId)/*{{{*/
+    {
+        if (!$forumId) {
+            trigger_error(
+                "Forum '{$forumId}' does not exist. " .
+                'Error Code: 96f0765076'
+            );
+        }
+    }/*}}}*/
+
+    private function rejectInvalidUser($userId, $username='')/*{{{*/
+    {
+        if (!$userId) {
+            trigger_error(
+                "Username '{$username}' does not exist. " .
+                'Error Code: 4781f662d8'
+            );
+        }
+    }/*}}}*/
+
+    public function unindexAllPostsByTopic($topicId)/*{{{*/
+    {
+        $search = $this->getSearchInstance();
+        $rowset = $this->getPostsRelatedToTopic($topicId);
+        $postIds = array_map(
+            function ($row) {
+                return $row['post_id'];
+            },
+            $rowset
+        );
+        $total = count($postIds);
+        $chunks = array_chunk($postIds, $this::CHUNK_SIZE);
+        foreach ($chunks as $chunk) {
+            $search->index_remove($chunk, [], []);
+        }
+        return $total;
+    }/*}}}*/
+
+    private function getPostsRelatedToTopic($topicId)/*{{{*/
+    {
+        $where[] = "a.topic_id={$topicId}";
+        $sql_ary = [
+            'SELECT'   => 'b.post_id',
+            'FROM'     => [TOPICS_TABLE => 'a'],
+            'LEFT_JOIN' => [
+                [
+                    'FROM'  => [POSTS_TABLE => 'b'],
+                    'ON'    => 'a.topic_id=b.topic_id',
+                ],
+            ],
+            'WHERE'    => $where,
+        ];
+        $sql    = $this->db->sql_build_query('SELECT', $sql_ary);
+        $result = $this->db->sql_query($sql);
+        $rowset = $this->db->sql_fetchrowset($result);
+        $this->db->sql_freeresult($result);
+        return $rowset;
+    }/*}}}*/
+}
