01fe3e95d3b4203f4af10899c43fb9daac5a28cc
johnelliotbaker <johnelliotbaker@gmail.com>
2020-08-06 23:35:32 -0700

    Implement new model fields
    JsonField and CommaSeparatedStringField

---
+++ core/Rest/Fields/CommaSeparatedStringField.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace jeb\snahp\core\Rest\Fields;
+
+class CommaSeparatedStringField extends Field
+{
+    public function validate($data)
+    {
+        $data = preg_replace('#[\s,]+#', ',', $data);
+        $data = preg_replace('#,$#', '', $data);
+        if ($data === '') {
+            return '';
+        }
+        $data = explode(',', $data);
+        return $this->serialize($data);
+    }
+
+    public function serialize($validData)
+    {
+        return implode(',', $validData);
+    }
+}
