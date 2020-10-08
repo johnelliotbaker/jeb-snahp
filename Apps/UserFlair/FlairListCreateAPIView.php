35daad39cfe42a9a7c9afd199a3e307c7a949d52
johnelliotbaker <johnelliotbaker@gmail.com>
2020-10-08 02:37:43 -0700

    Add permission classes to user flair API

--- Apps/UserFlair/FlairListCreateAPIView.php
+++ Apps/UserFlair/FlairListCreateAPIView.php
@@ -3,45 +3,84 @@
 namespace jeb\snahp\Apps\UserFlair;
 
 require_once '/var/www/forum/ext/jeb/snahp/core/Rest/Views/Generics.php';
-require_once '/var/www/forum/ext/jeb/snahp/Apps/UserFlair/Models/Flair.php';
+require_once '/var/www/forum/ext/jeb/snahp/core/Rest/Permissions/Permission.php';
 
-use jeb\snahp\core\Rest\Views\ListCreateAPIView;
 use jeb\snahp\core\Rest\Serializers\Serializer;
-use jeb\snahp\Apps\UserFlair\Models\Flair;
-
-class MySerializer extends Serializer
-{
-    public function __construct($instance=null, $data=null, $kwargs=[])
-    {
-        parent::__construct($instance, $data, $kwargs);
-        $this->model = new Flair();
-    }
-}
+use jeb\snahp\core\Rest\Views\ListCreateAPIView;
+use jeb\snahp\core\Rest\Permissions\AllowDevPermission;
+use jeb\snahp\core\Rest\Permissions\AllowAnyPermission;
 
 
 class FlairListCreateAPIView extends ListCreateAPIView
 {
-    protected $serializerClass = __NAMESPACE__ . '\MySerializer';
+    protected $serializerClass = 'jeb\snahp\core\Rest\Serializers\ModelSerializer';
     protected $request;
     protected $sauth;
 
-    public function __construct($request, $sauth)
+    public function __construct($request, $sauth, $Flair)
     {
         $this->request = $request;
         $this->sauth = $sauth;
-        $sauth->reject_non_dev('Error Code: bb3484c715');
-        $this->model = new Flair();
+        $this->model = $Flair;
+        $this->permissionClasses = [
+            // TODO: Remove after testing
+            new AllowAnyPermission($sauth),
+            new AllowDevPermission($sauth),
+        ];
     }
 
     public function view()
     {
         return parent::dispatch();
     }
-
-
-    // public function create($request)/*{{{*/
-    // {
-    //     // $this->sauth->reject_non_dev('Error Code: 11e18d40f8');
-    //     return parent::create($request);
-    // }/*}}}*/
 }
+
+
+
+
+
+
+
+
+
+
+// <?php
+//
+// namespace jeb\snahp\Apps\UserFlair;
+//
+// require_once '/var/www/forum/ext/jeb/snahp/core/Rest/Views/Generics.php';
+// require_once '/var/www/forum/ext/jeb/snahp/Apps/UserFlair/Models/Flair.php';
+//
+// use jeb\snahp\core\Rest\Views\ListCreateAPIView;
+// use jeb\snahp\core\Rest\Serializers\Serializer;
+// use jeb\snahp\Apps\UserFlair\Models\Flair;
+//
+// class MySerializer extends Serializer
+// {
+//     public function __construct($instance=null, $data=null, $kwargs=[])
+//     {
+//         parent::__construct($instance, $data, $kwargs);
+//         $this->model = new Flair();
+//     }
+// }
+//
+//
+// class FlairListCreateAPIView extends ListCreateAPIView
+// {
+//     protected $serializerClass = __NAMESPACE__ . '\MySerializer';
+//     protected $request;
+//     protected $sauth;
+//
+//     public function __construct($request, $sauth)
+//     {
+//         $this->request = $request;
+//         $this->sauth = $sauth;
+//         $sauth->reject_non_dev('Error Code: bb3484c715');
+//         $this->model = new Flair();
+//     }
+//
+//     public function view()
+//     {
+//         return parent::dispatch();
+//     }
+// }
--- Apps/UserFlair/TypeListCreateAPIView.php
+++ Apps/UserFlair/TypeListCreateAPIView.php
@@ -3,58 +3,95 @@
 namespace jeb\snahp\Apps\UserFlair;
 
 require_once '/var/www/forum/ext/jeb/snahp/core/Rest/Views/Generics.php';
-require_once '/var/www/forum/ext/jeb/snahp/Apps/UserFlair/Models/Type.php';
+require_once '/var/www/forum/ext/jeb/snahp/core/Rest/Permissions/Permission.php';
 
-use jeb\snahp\core\Rest\Views\ListCreateAPIView;
 use jeb\snahp\core\Rest\Serializers\Serializer;
-use jeb\snahp\Apps\UserFlair\Models\Type;
-
-class MySerializer extends Serializer
-{
-    public function __construct($instance=null, $data=null, $kwargs=[])
-    {
-        parent::__construct($instance, $data, $kwargs);
-        $this->model = new Type();
-    }
-}
+use jeb\snahp\core\Rest\Views\ListCreateAPIView;
+use jeb\snahp\core\Rest\Permissions\AllowDevPermission;
+use jeb\snahp\core\Rest\Permissions\AllowAnyPermission;
 
 
 class TypeListCreateAPIView extends ListCreateAPIView
 {
-    protected $serializerClass = __NAMESPACE__ . '\MySerializer';
+    protected $serializerClass = 'jeb\snahp\core\Rest\Serializers\ModelSerializer';
     protected $request;
     protected $sauth;
 
-    public function __construct($request, $sauth)
+    public function __construct($request, $sauth, $Type)
     {
         $this->request = $request;
         $this->sauth = $sauth;
-        $sauth->reject_non_dev('Error Code: 729077e8ca');
-        $this->model = new Type();
+        $this->model = $Type;
+        $this->permissionClasses = [
+            // TODO: Remove after testing
+            new AllowAnyPermission($sauth),
+            new AllowDevPermission($sauth),
+        ];
     }
 
     public function view()
     {
         return parent::dispatch();
     }
-
-
-    public function getQueryset()/*{{{*/
-    {
-        $data = getRequestData($this->request);
-        if (!$data) {
-            return array_values(\R::find($this->model::TABLE_NAME));
-        }
-        foreach ($data as $varname => $value) {
-            $sqlAry[] = "${varname}='${value}'";
-        }
-        $where = implode(' AND ', $sqlAry);
-        return array_values(\R::find($this->model::TABLE_NAME, $where));
-    }/*}}}*/
-
-    public function create($request)/*{{{*/
-    {
-        // $this->sauth->reject_non_dev('Error Code: 11e18d40f8');
-        return parent::create($request);
-    }/*}}}*/
 }
+
+// <?php
+//
+// namespace jeb\snahp\Apps\UserFlair;
+//
+// require_once '/var/www/forum/ext/jeb/snahp/core/Rest/Views/Generics.php';
+// require_once '/var/www/forum/ext/jeb/snahp/Apps/UserFlair/Models/Type.php';
+//
+// use jeb\snahp\core\Rest\Views\ListCreateAPIView;
+// use jeb\snahp\core\Rest\Serializers\Serializer;
+// use jeb\snahp\Apps\UserFlair\Models\Type;
+//
+// class MySerializer extends Serializer
+// {
+//     public function __construct($instance=null, $data=null, $kwargs=[])
+//     {
+//         parent::__construct($instance, $data, $kwargs);
+//         $this->model = new Type();
+//     }
+// }
+//
+//
+// class TypeListCreateAPIView extends ListCreateAPIView
+// {
+//     protected $serializerClass = __NAMESPACE__ . '\MySerializer';
+//     protected $request;
+//     protected $sauth;
+//
+//     public function __construct($request, $sauth)
+//     {
+//         $this->request = $request;
+//         $this->sauth = $sauth;
+//         $sauth->reject_non_dev('Error Code: 729077e8ca');
+//         $this->model = new Type();
+//     }
+//
+//     public function view()
+//     {
+//         return parent::dispatch();
+//     }
+//
+//
+//     public function getQueryset()/*{{{*/
+//     {
+//         $data = getRequestData($this->request);
+//         if (!$data) {
+//             return array_values(\R::find($this->model::TABLE_NAME));
+//         }
+//         foreach ($data as $varname => $value) {
+//             $sqlAry[] = "${varname}='${value}'";
+//         }
+//         $where = implode(' AND ', $sqlAry);
+//         return array_values(\R::find($this->model::TABLE_NAME, $where));
+//     }/*}}}*/
+//
+//     public function create($request)/*{{{*/
+//     {
+//         // $this->sauth->reject_non_dev('Error Code: 11e18d40f8');
+//         return parent::create($request);
+//     }/*}}}*/
+// }
