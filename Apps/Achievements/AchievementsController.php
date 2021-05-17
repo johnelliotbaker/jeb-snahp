f70204bdcb392c82b0b770bcc57d2ae68fe6ff0a
johnelliotbaker <johnelliotbaker@gmail.com>
2021-05-17 03:03:09 -0700

    Show ranked icons after username

---
+++ Apps/Achievements/AchievementsController.php
@@ -0,0 +1,49 @@
+<?php
+namespace jeb\snahp\Apps\Achievements;
+
+use \Symfony\Component\HttpFoundation\Response;
+use \Symfony\Component\HttpFoundation\JsonResponse;
+
+class AchievementsController
+{
+    protected $db;/*{{{*/
+    protected $user;
+    protected $config;
+    protected $request;
+    protected $template;
+    protected $container;
+    protected $phpHelper;
+    protected $tbl;
+    protected $sauth;
+    protected $helper;
+    public function __construct(
+        $db,
+        $user,
+        $config,
+        $request,
+        $template,
+        $container,
+        $phpHelper,
+        $tbl,
+        $sauth,
+        $helper
+    ) {
+        $this->db = $db;
+        $this->user = $user;
+        $this->config = $config;
+        $this->request = $request;
+        $this->template = $template;
+        $this->container = $container;
+        $this->phpHelper = $phpHelper;
+        $this->tbl = $tbl;
+        $this->sauth = $sauth;
+        $this->helper = $helper;
+        $this->userId = $sauth->userId;
+        $this->sauth->reject_anon('Error Code: a5e8ee80c7');
+    }/*}}}*/
+
+    public function view()/*{{{*/
+    {
+        return new Response('Hello World');
+    }/*}}}*/
+}
---
+++ Apps/Achievements/AchievementsHelper.php
@@ -0,0 +1,61 @@
+<?php
+namespace jeb\snahp\Apps\Achievements;
+
+class AchievementsHelper
+{
+    protected $db;/*{{{*/
+    protected $user;
+    protected $template;
+    protected $tbl;
+    protected $sauth;
+    public function __construct(
+        $db,
+        $tbl,
+        $sauth,
+        $pageNumberPagination,
+        $QuerySetFactory,
+        $ToplistHelper
+    ) {
+        $this->db = $db;
+        $this->tbl = $tbl;
+        $this->sauth = $sauth;
+        $this->paginator = $pageNumberPagination;
+        $this->QuerySetFactory = $QuerySetFactory;
+        $this->ToplistHelper = $ToplistHelper;
+        $this->userId = $sauth->userId;
+    }/*}}}*/
+
+    public function getAchievements($userId)/*{{{*/
+    {
+        $res = [];
+        $rep = $this->ToplistHelper->getReputationToplist();
+        $count = 0;
+        foreach ($rep as $k => $v) {
+            if ((int) $v['user_id'] === $userId) {
+                $res['reputation'] = $k + 1;
+                $count++;
+                break;
+            }
+        }
+        $req = $this->ToplistHelper->getRequestSolvedToplist();
+        foreach ($req as $k => $v) {
+            if ((int) $v['user_id'] === $userId) {
+                $res['requests_solved'] = $k + 1;
+                $count++;
+                break;
+            }
+        }
+        $thanks = $this->ToplistHelper->getThanksToplist();
+        foreach ($thanks as $k => $v) {
+            if ((int) $v['user_id'] === $userId) {
+                $res['thanks'] = $k + 1;
+                $count++;
+                break;
+            }
+        }
+        if ($count > 0) {
+            $res['count'] = $count;
+        }
+        return $res;
+    }/*}}}*/
+}
