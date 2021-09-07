092f09eb74300c6ce42601059285fc83c324d12c
johnelliotbaker <johnelliotbaker@gmail.com>
2021-09-07 02:22:45 -0700

    Improve db/query mixin

---
+++ Apps/Core/Db/query/Mixins.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace jeb\snahp\Apps\Core\Db\query;
+
+trait BaseQueryMixin
+{
+    public function update($id, $data)/*{{{*/
+    {
+        $id = (int) $id;
+        $idString = $this->ID_STRING;
+        $sql = 'UPDATE ' . $this->OWN_TABLE_NAME . '
+            SET ' . $this->db->sql_build_array('UPDATE', $data) . "
+            WHERE ${idString}=${id}";
+        $this->db->sql_query($sql);
+    }/*}}}*/
+
+    public function get($id, $options=null)/*{{{*/
+    {
+        $fields = $options['fields'] ?? ['*'];
+
+        $id = (int) $id;
+        $sqlArray = [
+            'SELECT' => $this->makeFieldString($fields, 'a'),
+            'FROM' => [$this->OWN_TABLE_NAME => 'a'],
+            'WHERE' => $this->ID_STRING . "=${id}",
+        ];
+        $sql = $this->db->sql_build_query('SELECT', $sqlArray);
+        $result = $this->db->sql_query($sql);
+        $row = $this->db->sql_fetchrow($result);
+        $this->db->sql_freeresult($result);
+        return $row;
+    }/*}}}*/
+
+    public function makeFieldString($fields, $tablename=null)/*{{{*/
+    {
+        if (is_string($fields)) {
+            $fields = explode(',', $fields);
+        }
+        if ($tablename) {
+            $fields = array_map(
+                function ($field) use ($tablename) {
+                    return "$tablename.$field";
+                },
+                $fields
+            );
+        }
+        return implode(', ', $fields);
+    }/*}}}*/
+
+}
