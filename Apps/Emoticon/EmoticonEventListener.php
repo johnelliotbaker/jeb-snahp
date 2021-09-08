ba7a1f0b236893e1b19f2d637e3af796dc5601b5
johnelliotbaker <johnelliotbaker@gmail.com>
2021-09-08 16:41:42 -0700

    Refactor emote
    Renamed to emoticon
    Doesn't make api call to the server

---
+++ Apps/Emoticon/EmoticonEventListener.php
@@ -0,0 +1,34 @@
+<?php
+namespace jeb\snahp\Apps\Emoticon;
+
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class EmoticonEventListener implements EventSubscriberInterface
+{
+    public function __construct(
+        $sauth,
+        $helper
+    ) {
+        $this->sauth = $sauth;
+        $this->helper = $helper;
+        $this->userId = $this->sauth->userId;
+    }
+
+    public static function getSubscribedEvents()/*{{{*/
+    {
+        return [
+            'core.viewtopic_assign_template_vars_before' => [
+                ['embedEmoticon', 1],
+            ],
+            'core.posting_modify_template_vars' => [
+                ['embedEmoticon', 1],
+            ],
+        ];
+    }/*}}}*/
+
+    public function embedEmoticon($event)
+    {
+        $this->helper->embedEmoticon($this->userId);
+    }
+}
---
+++ Apps/Emoticon/EmoticonHelper.php
@@ -0,0 +1,55 @@
+<?php
+namespace jeb\snahp\Apps\Emoticon;
+
+class EmoticonHelper
+{
+    public function __construct(
+        $db,
+        $template,
+        $tbl,
+        $definition,
+        $sauth
+    ) {
+        $this->db = $db;
+        $this->tbl = $tbl;
+        $this->template = $template;
+        $this->definition = $definition;
+        $this->sauth = $sauth;
+        $this->paginator = $pageNumberPagination;
+        $this->userId = $sauth->userId;
+    }
+
+    public function embedEmoticon($userId)/*{{{*/
+    {
+        // /var/www/forum/ext/jeb/snahp/styles/all/template/emotes/base.html
+        // <span id="rx_emoticon" {EMOTICON_PROPS}></span>
+        $data = $this->getEmoticon($userId);
+        $attributeData = convertArrayToHTMLAttribute($data);
+        $tplData = ['EMOTICON_PROPS' => "data-data=\"$attributeData\""];
+        $this->template->assign_vars($tplData);
+    }/*}}}*/
+
+    public function getEmoticon($userId)/*{{{*/
+    {
+        $data = [];
+        $groupId = $this->sauth->user->data['group_id'];
+        $groupData = $this->selectGroup($groupId);
+        $allowedTypes = unserialize($groupData['snp_emo_allowed_types']);
+        foreach ($this->definition as $key=>$entry) {
+            if (in_array($entry['type'], $allowedTypes)) {
+                $data[] = [ 'name' => $key ] + $entry;
+            }
+        }
+        return $data;
+    }/*}}}*/
+
+
+    public function selectGroup($gid)/*{{{*/
+    {
+        $sql = 'SELECT * FROM ' . GROUPS_TABLE . " WHERE group_id=$gid";
+        $result = $this->db->sql_query($sql);
+        $row = $this->db->sql_fetchrow($result);
+        $this->db->sql_freeresult($result);
+        return $row;
+    }/*}}}*/
+}
