9050f0bb683442328b5e97ff8dca1d1c3df99082
johnelliotbaker <johnelliotbaker@gmail.com>
2021-09-08 16:36:26 -0700

    Improve boilerplate

--- Apps/Boilerplate/BoilerplateEventListener.php
+++ Apps/Boilerplate/BoilerplateEventListener.php
@@ -6,21 +6,15 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class BoilerplateEventListener implements EventSubscriberInterface
 {
-    protected $user;
-    protected $config;
-    protected $sauth;
-    protected $helper;
     public function __construct(/*{{{*/
-        $user,
         $config,
         $sauth,
         $helper
     ) {
-        $this->user = $user;
         $this->config = $config;
         $this->sauth = $sauth;
         $this->helper = $helper;
-        $this->user_id = $this->user->data['user_id'];
+        $this->userId = $this->sauth->userId;
     }/*}}}*/
 
     public static function getSubscribedEvents()/*{{{*/
